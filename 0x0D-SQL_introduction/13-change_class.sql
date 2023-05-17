415402382b400c5d77c8cf93176de6577423fc1f
KabiruA159 <kaabdullahi12@gmail.com git config --global user.name KabiruA159l.com>
2023-05-16 21:16:33 -0700

    tasks 13

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,4 @@
--- displays the max temperature of each state (ordered by State name).
+-- removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- The database name will be passed as an argument of the mysql command
 
-SELECT state, MAX(value) as max_temp FROM temperatures GROUP BY state ORDER BY state;
+DELETE FROM second_table WHERE score <=5;
